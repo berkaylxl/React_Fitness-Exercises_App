7dfe73be2a0ad65749f80a080e07d823de5cabb7
Berkaycan Sakar <berkaycansakar@gmail.com>
2023-03-09 01:25:01 +0300

    Search bar style completed

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,8 +1,61 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Box, Button, Stack, TextField, Typography } from '@mui/material'
+import { fontSize, height } from '@mui/system'
 
 const SearchExercises = () => {
+  const [search, setSearch] = useState('')
+
+  const handleSearch = async () => {
+   if(search){
+    // const exerciseData = await fetchData();
+   } 
+  }
   return (
-    <div>SearchExercises</div>
+    <Stack alignItems="center" mt="37px"
+      justifyContent="center" p="20px">
+      <Typography fontWeight={700} sx={{
+        fontSize: { lg: '44px', xs: '30px' }
+      }} mb="50px" textAlign="center">
+        Awesome Exercise You<br /> Should Know
+      </Typography>
+      <Box position="relative" mb="72px">
+        <TextField
+          sx={{
+            input: {
+              fontWeight: '700',
+              border: 'none',
+              borderRadius: '4px'
+            },
+            width: { lg: '800px', xs: '350px' },
+            backgroundColor: '#fff',
+            borderRadius: '40px'
+          }}
+          height="76px"
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
+          placeholder="Search Exercise"
+          type="Text"
+        />
+        <Button className='search-btn'
+          sx={{
+            bgcolor: '#FF2625',
+            color: '#fff',
+            textTransform: 'none',
+            width: { lg: '175px', sm: '120px', xs: '350px' },
+            fontSize: { lg: '20px', xs: '14px' },
+            height: '56px',
+            position: { lg: 'absolute', xs: 'static' },
+            marginTop: { lg: '0px', sm: '0', xs: '10px' },
+            right: '0'
+          }}
+          onClick={handleSearch}>
+          Search
+        </Button>
+
+
+
+      </Box>
+    </Stack >
   )
 }
 
